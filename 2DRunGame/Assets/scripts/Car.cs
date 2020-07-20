9d7fdc30050bd5be87275b91b63ca5fce7529f49
karta1026959 <67904641+karta1026959@users.noreply.github.com>
2020-07-20 21:28:51 +0800

    完成特效、物理引擎

---
+++ 2DRunGame/Assets/scripts/Car.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    /*
+     int.float.string.bool
+     */
+
+    string brand ;
+    int cc;
+    float weight;
+    bool window;
+    float speed;
+    string color;
+
+}
